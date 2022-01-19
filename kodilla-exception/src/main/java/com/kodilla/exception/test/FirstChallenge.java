e2190340e78897f6ad08b9deef61a821e64967b3
Kasia Tunska <k.j.tunska@gmail.com>
2022-01-19 22:40:41 +0100

    8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,34 @@
+package com.kodilla.exception.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = 0;
+
+
+        try {
+            result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Error b=0");
+        } finally {
+            System.out.println("The result a/b: " + result);
+
+
+
+        }
+    }
+}

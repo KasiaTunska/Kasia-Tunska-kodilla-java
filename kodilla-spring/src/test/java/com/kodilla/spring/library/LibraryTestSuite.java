e3c3afecdec9ec5c3503dc7a574d781c7c31c0a9
Kasia Tunska <k.j.tunska@gmail.com>
2022-02-01 21:39:36 +0100

    15.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,66 @@
+package com.kodilla.spring.library;
+
+import com.kodilla.spring.reader.ReaderConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LibraryTestSuite {
+
+    @Test
+    void testLoadFromDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.loadFromDb();
+
+        //Then
+        //do nothing
+    }
+
+    @Test
+    void testSaveToDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.saveToDb();
+
+        //Then
+        //do nothing
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
+    }
+    @Test
+    void testConditional() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(ReaderConfig.class);
+        //When
+        boolean book2Exists = context.containsBean("book2");
+        //Then
+        System.out.println("Bean book2 was found in the container: " + book2Exists);
+    }
+}

1313d92fba5ed47354d00278a68c73f8debb1d0a
Kasia Tunska <k.j.tunska@gmail.com>
2022-02-02 17:25:36 +0100

    wzorzec strategy

---
+++ spring-web/src/main/java/com/kodilla/patterns/strategy/IndividualCustomer.java
@@ -0,0 +1,13 @@
+package com.kodilla.patterns.strategy;
+
+
+
+
+
+public class IndividualCustomer extends Customer {
+
+    public IndividualCustomer(String name) {
+        super(name);
+        this.buyPredictor = new ConservativePredictor();
+    }
+}

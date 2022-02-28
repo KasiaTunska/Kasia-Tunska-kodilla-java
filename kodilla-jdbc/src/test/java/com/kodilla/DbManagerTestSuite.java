935c28cfb2a7f6ab86061b2f9ce8b3ea0ffb43fe
Kasia Tunska <k.j.tunska@gmail.com>
2022-02-28 09:24:51 +0100

    16.6

--- kodilla-jdbc/src/test/java/com/kodilla/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/DbManagerTestSuite.java
@@ -24,6 +24,7 @@ class DbManagerTestSuite {
         assertNotNull(dbManager.getConnection());
     }
     @Test
+
     void testSelectUsers() throws SQLException {
         //Given
         DbManager dbManager = DbManager.getInstance();

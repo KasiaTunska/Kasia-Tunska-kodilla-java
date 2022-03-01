2247e395b72bed30a898241b3b010fdaae92a7bf
Kasia Tunska <k.j.tunska@gmail.com>
2022-03-01 21:33:15 +0100

    17.1

---
+++ kodilla-hibernate/src/test/java/kodillahibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,14 @@
+package kodillahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
